07d7e7bd94a13900261a9626123434a5d5a4cb4a
Vivien Denizart <vivien.denizart@gmail.com>
2022-01-23 01:15:00 +0100

    compatibility namespace SF 3

--- src/Controller/SynchroController.php
+++ src/Controller/SynchroController.php
@@ -2,7 +2,7 @@
 
 namespace Parallalax\SynchroBundle\Controller;
 
-use App\Entity\User;
+use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
