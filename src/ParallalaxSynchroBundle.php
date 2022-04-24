7e994a47a74b2686321a61e6b695f41415aab3ff
vDenizart <vivien.denizart@gmail.com>
2022-04-25 01:39:21 +0200

    Update ParallalaxSynchroBundle.php

--- src/ParallalaxSynchroBundle.php
+++ src/ParallalaxSynchroBundle.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ParallalaxSynchroBundle extends Bundle {
 
-    public function getContainerExtension(): ?Symfony\Component\DependencyInjection\Extension\ExtensionInterface
+    public function getContainerExtension(): ?\Symfony\Component\DependencyInjection\Extension\ExtensionInterface
     {
         if (null === $this->extension) {
             $this->extension = new ParallalaxSynchroExtension();
